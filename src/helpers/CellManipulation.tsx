833fffea509f579deed83e94461eacffe0b50a35
Nzeshi Sarah <sarahnzeshi05@gmail.com>
2025-06-27 01:17:39 +0100

    added a function to get neighbours

--- src/helpers/CellManipulation.tsx
+++ src/helpers/CellManipulation.tsx
@@ -6,3 +6,20 @@ export const incrementNeighbours = (coords: Coord, field: Field): Field=>{
     return field
     console.log(coords)
 }
+
+export const getNeighbours = ([x, y]: Coord) : Record<string, [number, number]>=>(
+    {
+        top: [x - 1, y],
+        topRight: [x - 1, y + 1],
+        right: [x, y + 1],
+        rightBottom: [x + 1, y + 1],
+        bottom: [x + 1, y],
+        bottomLeft: [x + 1, y - 1],
+        left: [x, y - 1],
+        leftTop: [x - 1, y - 1]
+    }
+)
+
+export const isIteminField = ([x, y]: Coord, {length}: Field): boolean=>{
+    return x >= 0 && y>=0 && length - x > 0 && length - y > 0
+}
--- src/test/CellManipulation.test.ts
+++ src/test/CellManipulation.test.ts
@@ -11,4 +11,5 @@ describe('Check increment neighbours', ()=>{
     })
 })
 
+
 console.log(empty)
